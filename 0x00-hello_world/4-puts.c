29df1599ff99512db2caecd8194269df16b68f1f
Olalekan <Olalekanabsm@gmail.com>
2022-06-08 00:36:38 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *  uses put method to print "programming is like building ..."
+ *   */
+
+int main(void)  
+{   
+	puts("\"Programming is like building a multilingual puzzle"):
+
+                    returns(0);
+
+}
