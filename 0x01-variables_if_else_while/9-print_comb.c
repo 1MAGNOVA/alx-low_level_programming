4cbcc4eedc664f3d0581515216299d95dede476f
olalekan <olalekanabsm@gmail.com>
2022-06-11 21:06:49 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - main block
+ * prints all single digit numbers of base 10
+ * followed by a new line.
+ * Return: Always 0 Success
+ */
+int main(void)
+{	int c = 0;
+	while (c < 10)
+	{
+		putchar(48 + c);
+		if (c != 9)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+		c++;
+	}putchar('\n');
+	return (0);
+}
