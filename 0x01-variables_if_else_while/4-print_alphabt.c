3124e7901391a75d7677d5f3dd79fb483b473061
olalekan <olalekanabsm@gmail.com>
2022-06-11 20:13:12 -0700

    4-prints

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Get random number and checks its last digit, compare it with 5
+ * Return: Always 0 Success
+ */
+int main(void)
+{
+	char c = 'a';
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+	{
+		putchar(c);
+	}
+		c++;
+	}
+		putchar('\n');
+			return (0);
+}
