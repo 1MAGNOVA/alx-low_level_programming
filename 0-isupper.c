1f9810c049b8f6ff8934c8e5e79c923c67291357
Olalekan <Olalekanabsm@gamil.com>
2022-09-15 18:15:45 -0700

    nested loop 0x04

---
+++ 0-isupper.c
@@ -0,0 +1,22 @@
+#include <main.h>
+/*
+ * main - Prototype: int _isupper(int c);
+ * Returns 1 if c is uppercase, Returns 0 otherwise
+ * Return 0 always
+ *
+ */
+int main(void)
+{
+	char c;
+	
+	c = 'A';
+	if {
+ 	putchar("%c: %d\n", c, _isupper(c));
+	
+		return(1);
+	else
+		return(0);
+	}
+		return(0);
+
+}
