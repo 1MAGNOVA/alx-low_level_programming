df9846e052d71bd9b80f6a10e3fba579c16af4d5
Olalekan <Olalekanabsm@gmail.com>
2022-09-18 14:05:06 -0700

    put problrm

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 #include <main.h>
 /**
- * main - prints _putchar with a new line
+ * 
+ * main - prints _putchar with a new line 
+ *returns 0
  *
  *
- * returns 0
- */
+ **/
 int main(void)
 {
+	char c = "putchar";
 
+	while (c)
+	{
+		_putchar(c); sh++;
+	} 
+	_putchar('\n');
 
-	putchar("_putchar \n")
-		return(0)
-
-
-
-
-
-
-
-
+	return (0);
 }
