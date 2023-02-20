03fcdea6d25bd82f13be4e1fd3c78841d1eb7bc2
olalekan <olalekanabsm@gmail.com>
2023-02-20 03:35:17 -0800

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
-/*
- * Main -  exactly and that piece of art is useful - Dora Korpar, 2015-10-19 in standard error.
+/**
+ * main - Prints out exactly and that piece of art is useful - Dora Korpar, 2015-10-19 in standard error.
  * Return:1 if success
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n", 58);
-		return(1);
+	write ("'and that piece of art is useful' - Dora Korpar, 2015 - 10 - 19\n");
+		return (1);
 }
 
