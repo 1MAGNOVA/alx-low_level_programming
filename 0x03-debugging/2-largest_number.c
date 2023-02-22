e751731c2d0ff17dde018fbe4d05ae993ba0c839
olalekan <olalekanabsm@gmail.com>
2023-02-22 06:48:47 -0800

    largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,2 +1,29 @@
-#!/bin/bash
-gcc -E -o c $CFILE
+#include <stdio.h>
+#include <main.h>
+/**
+ * main - returns the largest number of the three numbers provided
+ * a: first int
+ * b: second int
+ * c: third int
+ * Return: largest number (success)
+ */
+
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if (a> b && b> c)
+	{
+		largest = a;
+	}
+	else if (b > a && a > c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+
+	return (largest);
+}
