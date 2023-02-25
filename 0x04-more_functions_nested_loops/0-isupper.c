0807f5565bbf773038dbcad732775b52aa8cefa5
olalekan <olalekanabsm@gmail.com>
2023-02-24 18:22:31 -0800

    is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,18 +5,10 @@
  * Return 0 always
  *
  */
-int main(void)
-{
-	char c;
-	
-	c = 'A';
-	if {
- 	putchar("%c: %d\n", c, _isupper(c));
-	
+int _isupper(int c)
+{	if ((c => 'A') &&(c <= 'Z'))
 		return(1);
 	else
-		return(0);
-	}
 		return(0);
 
 }
