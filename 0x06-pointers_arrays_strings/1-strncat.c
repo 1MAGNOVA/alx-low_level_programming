02a313ffe8526f563a3c0691e94a5802f06dd8ef
olalekan <olalekanabsm@gmail.com>
2023-03-01 07:14:45 -0800

    1-strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,11 +9,7 @@
  * Return: dest
  */
 char *_strmcat(char *dest, char *src, int n)
-{
-	int i;
-	int j;
-
-	i = 0;
+{	int i;int j;i = 0;
 	while (dest[i] != '\0')
 	{
 		i++;
@@ -21,9 +17,9 @@ char *_strmcat(char *dest, char *src, int n)
 	j = 0;
 	while (j < n && src[j] != '\0')
 	{
-	dest[i] = src[j];
-	i++;
-	j++;
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
 	dest[i] = '\0';
 	return (dest);
