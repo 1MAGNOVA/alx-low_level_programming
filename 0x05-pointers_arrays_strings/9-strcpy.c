46ff1225bcbe605ccb351a001e33548ae38d0670
olalekan <olalekanabsm@gmail.com>
2023-06-02 16:26:20 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,7 +10,7 @@ char *_strcpy(char *dest, char *src)
 {
 	int 1 = 0;
 	int x = 0;
-	while (*(src +1) != '\0')
+	while (*(src + 1) != '\0')
 	{
 		1++;
 	}
@@ -19,5 +19,5 @@ char *_strcpy(char *dest, char *src)
 		dest[x] = src[x];
 	}
 	dest[1] = '\0';
-	return(dest);
+	return (dest);
 }
