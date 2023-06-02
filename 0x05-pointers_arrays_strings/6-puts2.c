a5e0582a810108467c080fa911ca26c8eaf8d0de
olalekan <olalekanabsm@gmail.com>
2023-06-02 14:43:08 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,7 +8,7 @@
 void puts2(char *str)
 {
 	int longi = 0;
-	int  t = 0;
+	int t = 0;
 	char *y = str;
 	int o;
 
@@ -21,7 +21,7 @@ void puts2(char *str)
 	for (o = 0 ; o <= t ; o++)
 	{
 		if ( o % 2 == 0)
-	}
+	{
 		_putchar(str[o]);
 	}
 	}
