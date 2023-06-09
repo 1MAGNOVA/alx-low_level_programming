42158c798dcac00c02b9c14501857f49181a60c1
olalekan <olalekanabsm@gmail.com>
2023-06-09 16:58:20 -0700

    0-puts_recursion.c

--- 0x08-recursion/_putchar.c
+++ 0x08-recursion/_putchar.c
@@ -3,9 +3,7 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c:
- * The character to print
- *
+ * @c: The character to print
  * Return: On Success1
  * On error; -1 is returned and errno is set appropriately
  */
