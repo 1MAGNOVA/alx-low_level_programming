9737cbe60e88e174b53763c5118422f8e0ceac34
olalekan <olalekanabsm@gmail.com>
2023-06-09 18:04:55 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,7 +12,7 @@ int _sqrt_recursion(int n)
 	return (actual_sqrt_recursion(n, 0));
 }
 /**
- * actual_sqrt_recursion recurses to find the natural square root of a number
+ * actual_sqrt_recursion - recurses to find the natural square root of a number
  * @n: number to calculate the root of
  * @i: the iterator
  * Return: the resulting square root
