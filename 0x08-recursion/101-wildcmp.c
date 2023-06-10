4aa062eedab0f70a2c1cf90be02bb092994d9766
olalekan <olalekanabsm@gmail.com>
2023-06-09 19:18:46 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -3,7 +3,7 @@
  * wildcmp - Compare strings
  * @s1: pointer to string params
  * @s2: pointer to string params
- * Return: 0 
+ * Return: 0
  */
 int wildcmp(char *s1, char *s2)
 {
@@ -18,11 +18,11 @@ int wildcmp(char *s1, char *s2)
 
 	if (*s2 == '*')
 	{
-		return(wildcmp(s1 + 1, s2) || wildcmp(s1, s2 +1));
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	}
 	else if (*s1 == *s2)
 	{
-		return(wildcmp(s1 + 1, s2 + 1));
+		return (wildcmp(s1 + 1, s2 + 1));
 	}
 	return (0);
 }
