4c971f8db25349d942dac71be4c907dacc5e9b9d
olalekan <olalekanabsm@gmail.com>
2023-06-09 17:58:04 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 void _putchar(char c);
+int actual_sqrt_recursion(int n, int i);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
