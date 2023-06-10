7f164ff0a4657310d2d7109956d1b106bd13deb7
olalekan <olalekanabsm@gmail.com>
2023-06-09 19:03:43 -0700

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -14,7 +14,7 @@ int is_palindrome(char *s)
 }
 /**
  * _strlen_recursion - returns the length of a string
- * @a: string to calculate the length of
+ * @s: string to calculate the length of
  * Return: length of the string
  */
 int _strlen_recursion(char *s)
