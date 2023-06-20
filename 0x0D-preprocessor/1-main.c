accac9027d18dc8eba5aed03c117611030afc8fa
olalekan <olalekanabsm@gmail.com>
2023-06-19 18:03:06 -0700

    0x0d preprocessor

--- 0x0D-preprocessor/1-main.c
+++ 0x0D-preprocessor/1-main.c
@@ -1,11 +1,13 @@
 #include "1-pi.h"
 #include "1-pi.h"
-#include <stdio.h>
+#include "stdio.h"
+
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
 	float a;
@@ -14,6 +16,6 @@ int main(void)
 	r = 98;
 	a = PI * r * r;
 	printf("%.3f\n", a);
-	
+       
 	return (0);
 }
