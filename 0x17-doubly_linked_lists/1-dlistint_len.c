b5ae733984f725c2eb31f9f46f6ecaa12c65047f
1MAGNOVA <olalekanabsm@gmail.com>
2023-08-24 19:50:35 -0700

    @all.c@

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints all elements of a
- * dlistint_t list
- * @h: head of the list
- * Return: the num of nodes
+ * dlistint_len - returns num of elements in
+ * a double linked lst
+ * @h: head of the lst
+ * Return: the number of nodes
  */
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	int count;
 
@@ -20,7 +20,6 @@ size_t print_dlistint(const dlistint_t *h)
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
 		count++;
 		h = h->next;
 	}
